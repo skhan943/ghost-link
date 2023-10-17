45846aebf0e1b5b6cb0ab5e47434eba24b785d23
Sufyan Khan <skhan943@uwo.ca>
2023-10-17 06:13:38 -0400

    Styling and text changes, home page

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -14,14 +14,14 @@ const Home = () => {
               Private conversations that won't haunt you 👻
             </h1>
             <p className="mb-8 text-xl font-normal text-gray-500 lg:text-2xl sm:px-16 xl:px-48">
-              There are some things that we don't want the world to see, trust
-              me, we don't wanna know...
+              There are some things that we don't want the world to see. At
+              GhostLink, we don't wanna know...
             </p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
               <Link to="/auth/signin">
                 <a
                   href=""
-                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100"
+                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 hover:ring-4 hover:ring-gray-100"
                 >
                   Sign in
                 </a>
@@ -29,7 +29,7 @@ const Home = () => {
               <Link to="/auth/register">
                 <a
                   href=""
-                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100"
+                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 hover:ring-4 hover:ring-gray-100"
                 >
                   Register
                 </a>
