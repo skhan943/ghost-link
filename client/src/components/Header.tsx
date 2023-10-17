1b5f865f438673482ea6652df24aa5833d1cfe06
Sufyan Khan <skhan943@uwo.ca>
2023-10-16 23:31:44 -0400

    Change header color

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <header className="bg-[#7E78C9] text-white py-4 text-center font-montserrat">
+    <header className="bg-[#443F84] text-white py-4 text-center font-montserrat">
       <h1 className="text-5xl font-bold">GhostLink</h1>
     </header>
   );
