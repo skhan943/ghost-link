8c3bf16feb64f60f5c22aacc02fac5c983565629
Sufyan Khan <skhan943@uwo.ca>
2023-10-16 22:42:38 -0400

    Tailwind CSS setup

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>This will be the Home page</h1>;
+  return <h1 className="text-3xl font-bold">This will be the Home page</h1>;
 };
 
 export default Home;
