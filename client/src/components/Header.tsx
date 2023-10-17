c1937e039ff2c8c66cc672909d932033b9b7a1c7
Sufyan Khan <skhan943@uwo.ca>
2023-10-16 23:04:28 -0400

    Implement header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <header className="bg-[#7E78C9] text-white py-4 text-center font-montserrat">
+      <h1 className="text-5xl font-bold">GhostLink</h1>
+    </header>
+  );
+};
+
+export default Home;
