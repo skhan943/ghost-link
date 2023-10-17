68580d9f6dd2dba81484925bbb6b7a7d08b33dbf
Sufyan Khan <skhan943@uwo.ca>
2023-10-16 23:31:59 -0400

    Implement basic home page design

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+import Header from "../components/Header";
+
 const Home = () => {
-  return <h1 className="text-3xl font-bold">This will be the Home page</h1>;
+  return (
+    <>
+      <body className="flex flex-col bg-[#282454] h-screen">
+        <Header></Header>
+        <section className="flex h-screen max-w-7xl items-center self-center">
+          <div className="text-center">
+            <h1 className="mb-4 text-4xl font-bold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+              Private conversations that won't haunt you 👻
+            </h1>
+            <p className="mb-8 text-xl font-normal text-gray-500 lg:text-2xl sm:px-16 xl:px-48">
+              There are some things that we don't want the world to see, trust
+              me, we don't wanna know...
+            </p>
+            <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+              <a
+                href="#"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100"
+              >
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100"
+              >
+                Register
+              </a>
+            </div>
+          </div>
+        </section>
+      </body>
+    </>
+  );
 };
 
 export default Home;
