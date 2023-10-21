71ade3c3adb3bffd8abeba83936f38488e05da81
Sufyan Khan <skhan943@uwo.ca>
2023-10-21 15:29:07 -0400

    Print db connection status

--- server/db.js
+++ server/db.js
@@ -1,7 +1,16 @@
 const pgp = require("pg-promise")();
 
-const connectionString =
-  "postgres://postgres:password@localhost:5432/database_name";
+const connectionString = "postgres://postgres:password@localhost:5432/postgres";
 const db = pgp(connectionString);
+
+// Print connection status
+db.connect()
+  .then((obj) => {
+    obj.done();
+    console.log("Database connection successful.");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -7,4 +7,4 @@ app.use("/api", routes);
 
 // Define port and start server
 const port = process.env.port || 8082;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}.`));
