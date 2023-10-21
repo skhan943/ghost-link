04a1e8eae02c03210d5bab51dc0587e1cae04b35
Sufyan Khan <skhan943@uwo.ca>
2023-10-21 15:41:03 -0400

    Configure CORS

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const routes = require("./routes");
 
 const app = express();
 
+// Configure routes
 app.use("/api", routes);
+
+// Configure CORS to allow requests from the same origin
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Define port and start server
 const port = process.env.port || 8082;
