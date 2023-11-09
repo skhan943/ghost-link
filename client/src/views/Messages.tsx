acbf8bf736c3ee6cb5ffaa05d48ffd69d80f4218
Sufyan Khan <skhan943@uwo.ca>
2023-11-09 09:37:34 -0500

    Add logout button to messages page

--- client/src/views/Messages.tsx
+++ client/src/views/Messages.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Header from "../components/Header";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../components/AuthContext";
 
 const Messages = () => {
-  return <h1>This will be the Messages page</h1>;
+  const navigate = useNavigate();
+
+  const { authState, logout } = useAuth(); // Get login method
+
+  console.log(authState);
+
+  const handleLogout = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://localhost:443/api/auth/logout",
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Logged out!");
+        logout();
+        navigate("/"); // Use navigate to redirect
+      }
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <body className="flex flex-col bg-[#282454] h-screen">
+        <Header linkTo="/messages"></Header>
+        <button onClick={handleLogout}>Logout</button>
+      </body>
+    </>
+  );
 };
 
 export default Messages;
