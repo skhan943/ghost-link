7836b26a11050bc38a9d91467a35a6dd66c5ebcc
Sufyan Khan <skhan943@uwo.ca>
2023-11-13 12:11:50 -0500

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 // Configure CORS to allow requests from the same origin
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://localhost:5173", credentials: true }));
 
 app.use(express.json()); // Body parser for JSON
 app.use(express.urlencoded({ extended: true })); // Body parser for URL-encoded data
