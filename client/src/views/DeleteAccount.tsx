9faa82e18fdee35b25b594f6e253bae55f0a7aa1
Sufyan Khan <skhan943@uwo.ca>
2023-12-07 17:06:23 -0500

    Implement delete account page

--- client/src/views/DeleteAccount.tsx
+++ client/src/views/DeleteAccount.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import Header from "../components/Header";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../components/AuthContext";
 
 const DeleteAccount = () => {
-  return <h1>This will be the Delete Account page</h1>;
+  const navigate = useNavigate();
+
+  const { logout } = useAuth(); // Get logout method
+
+  const handleDelete = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.delete("https://localhost:443/api/delete", {
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        alert("Deleted successfully!");
+        logout();
+        navigate("/"); // Use navigate to redirect
+      }
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <body className="flex flex-col bg-[#282454] h-screen">
+        <Header linkTo="/messages"></Header>
+        <Link to={"/messages"}>no</Link>
+        <button onClick={handleDelete}>yes</button>
+      </body>
+    </>
+  );
 };
 
 export default DeleteAccount;
